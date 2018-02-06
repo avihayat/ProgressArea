a9e3f470aa84f1b965ce685c5325e9636d3a9ef3
avih <avih.graffiti@gmail.com>
2018-02-06 12:39:03 +0200

    remove unused using

--- ProgressArea/Pages/ColumnWidth_StaticHeader_Scrolling_fullheight.aspx.cs
+++ ProgressArea/Pages/ColumnWidth_StaticHeader_Scrolling_fullheight.aspx.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using Telerik.Web.UI;
 
 public partial class ColumnWidth_StaticHeader_Scrolling_fullheight : System.Web.UI.Page
